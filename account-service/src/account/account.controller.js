a88b5d1b22a30d6e300f04d0e1562b29731f250f
dvelasquez-2024226 <dvelasquez-2024226@kinal.edu.gt>
2026-02-24 18:16:40 -0600

    chore:Se agregaron endpoints para poder unir el deposito a la cuenta

--- account-service/src/account/account.controller.js
+++ account-service/src/account/account.controller.js
@@ -1,6 +1,7 @@
 import { createAccountRecord } from "./account.service.js";
 import { getAccountsByUser } from './account.service.js';
 import { getAccountBalance } from './account.service.js';
+import Account from './account.model.js';
 
 
 export const createAccount = async (req, res) => {
@@ -62,5 +63,57 @@ export const getBalance = async (req, res, next) => {
         });
     } catch (err) {
         next(err);
+    }
+};
+
+export const getBalanceInternal = async (req, res) => {
+    try {
+        const { accountNumber } = req.params;
+
+        const account = await Account.findOne({ accountNumber });
+
+        if (!account) {
+            return res.status(404).json({
+                message: 'Cuenta no encontrada'
+            });
+        }
+
+        res.json({
+            accountNumber: account.accountNumber,
+            balance: account.balance,
+            _id: account._id
+        });
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+export const updateBalanceInternal = async (req, res) => {
+    try {
+        const { amount, type } = req.body;
+        const { accountNumber } = req.params;
+
+        const account = await Account.findOne({ accountNumber });
+        if (!account) throw new Error('Cuenta no encontrada');
+
+        if (type === 'DEPOSIT') {
+            account.balance += amount;
+        }
+
+        if (type === 'WITHDRAW') {
+            if (account.balance < amount)
+                throw new Error('Fondos insuficientes');
+
+            account.balance -= amount;
+        }
+
+        await account.save();
+
+        res.json(account);
+
+    } catch (err) {
+        res.status(400).json({ message: err.message });
     }
 };
