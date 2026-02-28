7ffdc4fef8623651b882ea18b53782a8fa6e753c
jmartinez-2024083 <jmartinez-2024083@kinal.edu.gt>
2026-02-28 12:59:11 -0600

    feat: agergar endpoint público de reporte admin de cuentas.

--- report-service/src/reports/report.routes.js
+++ report-service/src/reports/report.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { accountsMostMovementsReportController } from './report.controller.js';
+import { accountsMostMovementsReportController, accountsAdminOverviewController } from './report.controller.js';
 import { validateJWT } from '../../middlewares/validate-JWT.js';
 
 const router = Router();
 
 // GET /reports/accounts-most-movements?order=asc|desc&limit=10
 router.get('/accounts-most-movements', validateJWT, accountsMostMovementsReportController);
+
+router.get('/admin/accounts-overview', validateJWT, accountsAdminOverviewController);
 
 export default router;
