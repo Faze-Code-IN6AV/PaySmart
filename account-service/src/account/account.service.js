272da976467601f3152149f7333db05130c894d5
jmartinez-2024083 <jmartinez-2024083@kinal.edu.gt>
2026-02-28 21:36:51 -0600

    feat: servicio de cuentas completo - validaciones, bd propia y limpieza general.

--- account-service/src/account/account.service.js
+++ account-service/src/account/account.service.js
@@ -1,13 +1,23 @@
 import Account from './account.model.js'
 
-export const createAccountRecord = async ({accountData}) => {
-    const data = {...accountData};
+export const createAccountRecord = async ({ accountData }) => {
+    const data = { ...accountData };
+
+    // Verificar si el usuario ya tiene una cuenta de ese tipo
+    const existing = await Account.findOne({ 
+        userId: data.userId, 
+        accountType: data.accountType 
+    });
+
+    if (existing) {
+        throw new Error(`Ya tienes una cuenta de tipo ${data.accountType}`);
+    }
 
     const account = new Account(data);
     await account.save();
-    
+
     return account;
-}
+};
 
 export const getAccountsByUser = async (userId) => {
     return await Account.find({ userId });
