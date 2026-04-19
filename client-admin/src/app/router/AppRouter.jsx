c873d98c590c5594e34371f7fa4b09694f85212f
jmartinez-2024083 <jmartinez-2024083@kinal.edu.gt>
2026-04-19 13:55:36 -0600

    chore: configuración inicial con Vite y estrcutura base.

---
+++ client-admin/src/app/router/AppRouter.jsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from 'react-router-dom'
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Patsmart</div>} />
+    </Routes>
+  )
+}
