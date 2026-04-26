3fc1503e04498e812858e85265de25326f6bf411
jmartinez-2024083 <jmartinez-2024083@kinal.edu.gt>
2026-04-25 18:36:54 -0600

    docs: agregar documentación Swagger para el favoriteaccounts-service.

---
+++ favoriteaccount-service/swagger.js
@@ -0,0 +1,35 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'PaySmart - Favorite Account Service API',
+            version: 'v1',
+            description: 'API de gestion de cuentas favoritas y transferencias rapidas para PaySmart.',
+            contact: {
+                name: 'FazeCode'
+            }
+        },
+        components: {
+            securitySchemes: {
+                Bearer: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                    description: 'Ingresa tu JWT token. Ejemplo: Bearer {token}'
+                }
+            }
+        },
+        security: [{ Bearer: [] }]
+    },
+    apis: ['./src/favoriteaccounts/favoriteAccount.routes.js']
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export const setupSwagger = (app, basePath) => {
+    app.use(`${basePath}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    console.log(`Swagger disponible en: ${basePath}/docs`);
+};
