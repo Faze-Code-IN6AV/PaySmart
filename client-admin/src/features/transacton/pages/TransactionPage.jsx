20d30a3b396b637265766a48c11d6ae0eed73a9e
dvelasquez-2024226 <dvelasquez-2024226@kinal.edu.gt>
2026-04-28 22:07:05 -0600

    feat: se agregó un botón de acceso a la vista y a la ruta protegida

---
+++ client-admin/src/features/transacton/pages/TransactionPage.jsx
@@ -0,0 +1,8 @@
+export const TransactionPage = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-64'>
+      <h1 className='text-2xl font-bold text-white'>Transacciones</h1>
+      <p className='mt-3 text-sm text-gray-300'>Esta vista está en construcción. Pronto podrás ver tus transacciones aquí.</p>
+    </div>
+  );
+};
