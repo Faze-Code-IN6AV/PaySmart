ba7c5eb22e5271c77fabe2a96c4064397e138552
jmartinez-2024083 <jmartinez-2024083@kinal.edu.gt>
2026-06-07 20:16:55 -0600

    refactor: eliminar función register y acción de registro del store.

--- client-admin/src/shared/api/auth.js
+++ client-admin/src/shared/api/auth.js
@@ -6,11 +6,6 @@ export const login = async (data) => {
   return await axiosAuth.post('/auth/login', data);
 };
 
-// POST /api/v1/auth/register (registro público)
-export const register = async (data) => {
-  return await axiosAuth.post('/auth/register', data);
-};
-
 // POST /api/v1/auth/verify-email
 export const verifyEmail = async (token) => {
   return await axiosAuth.post('/auth/verify-email', { token });
